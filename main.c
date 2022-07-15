e66f5abf53b7438ea5d309681e2f8cb3a4ed1909
Sara <sfedlu28@gmail.com>
2022-07-15 13:20:58 -0700

    printf

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include <limits.h>
-
 #include <stdio.h>
-
 #include "main.h"
 
 
@@ -80,7 +78,6 @@ int main(void)
   
   _printf("Unknown:[%r]\n");
   
-  printf("Unknown:[%r]\n");
   
   return (0);
   
